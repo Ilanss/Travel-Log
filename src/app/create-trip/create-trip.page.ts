26e9bd48536dd9283bc976dfb513d532107ffd9f
Teo Colomberotto <teo.colomberotto@heig-vd.ch>
2019-12-12 11:03:23 +0100

    createTrip_backArrow

--- src/app/create-trip/create-trip.page.ts
+++ src/app/create-trip/create-trip.page.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-create-trip',
-  templateUrl: './create-trip.page.html',
-  styleUrls: ['./create-trip.page.scss'],
+	selector: 'app-create-trip',
+	templateUrl: './create-trip.page.html',
+	styleUrls: [ './create-trip.page.scss' ]
 })
 export class CreateTripPage implements OnInit {
+	constructor(private router: Router) {}
 
-  constructor() { }
+	ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+	back() {
+		this.router.navigateByUrl('/home/trip-list');
+	}
 }
