836f245c1f10760921c108987a04febd485443a8
Teo Colomberotto <teo.colomberotto@heig-vd.ch>
2019-12-16 16:10:17 +0100

    Preview_map

--- src/app/home/trip-list/trip-list.page.ts
+++ src/app/home/trip-list/trip-list.page.ts
@@ -3,6 +3,8 @@ import { AuthService } from 'src/app/auth/auth.service';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx';
+import { latLng, MapOptions, tileLayer } from 'leaflet';
 
 @Component({
 	selector: 'app-trip-list',
@@ -10,12 +12,21 @@ import { Router } from '@angular/router';
 	styleUrls: [ './trip-list.page.scss' ]
 })
 export class TripListPage implements OnInit {
+	mapOptions: MapOptions;
+
 	constructor(
 		private auth: AuthService,
 		// TODO: inject the HTTP client.
 		public http: HttpClient,
+		private geolocation: Geolocation,
 		private router: Router
-	) {}
+	) {
+		this.mapOptions = {
+			layers: [ tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18 }) ],
+			zoom: 13,
+			center: latLng(46.778186, 6.641524)
+		};
+	}
 
 	ionViewDidLoad() {
 		// TODO: make an HTTP request to retrieve the trips.
@@ -30,6 +41,16 @@ export class TripListPage implements OnInit {
 		this.http.get(url).subscribe((trips) => {
 			console.log(`Trips loaded`, trips);
 		});
+		//geocalisation
+		this.geolocation
+			.getCurrentPosition()
+			.then((position: Geoposition) => {
+				const coords = position.coords;
+				console.log(`User is at ${coords.longitude}, ${coords.latitude}`);
+			})
+			.catch((err) => {
+				console.warn(`Could not retrieve user position because: ${err.message}`);
+			});
 	}
 
 	newTrip() {
