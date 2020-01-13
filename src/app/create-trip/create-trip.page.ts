ba305f20cc8c61e9f2b53499fb1a211e89848541
Teo Colomberotto <teo.colomberotto@heig-vd.ch>
2020-01-13 13:49:17 +0100

    DelateTrip_implemented

--- src/app/create-trip/create-trip.page.ts
+++ src/app/create-trip/create-trip.page.ts
@@ -23,8 +23,7 @@ export class CreateTripPage implements OnInit {
 		public http: HttpClient,
 		private router: Router,
 		private createTripService: CreateTripService
-	)
-	{
+	) {
 		this.tripRequest = new TripRequest();
 	}
 
@@ -43,7 +42,6 @@ export class CreateTripPage implements OnInit {
 		this.tripError = false;
 
 		// Perform the authentication request to the API.
-<<<<<<< HEAD
 		this.createTripService.create(this.tripRequest).pipe(first()).subscribe({
 			next: () => {
 				this.router.navigateByUrl('/home/trip-list');
@@ -51,22 +49,8 @@ export class CreateTripPage implements OnInit {
 			error: (err) => {
 				console.log(this.tripRequest);
 				this.tripError = true;
-				console.warn(`Authentication failed: ${err.message}`);
+				console.warn(`Trip creation failed: ${err.message}`);
 			}
 		});
-=======
-		this.createTripService.create(this.tripRequest)
-			.pipe(first())
-			.subscribe({
-				next: () => {
-					this.router.navigateByUrl('/home/trip-list');
-				},
-				error: err => {
-					console.log(this.tripRequest)
-					this.tripError = true;
-					console.warn(`Trip creation failed: ${err.message}`);
-				}
-			});
->>>>>>> be4d4e0730addc949ab9eaca6d464237cf393f36
 	}
 }
