0e80ba2eb52884b854692c4d3cc9f87c9b4de45b
Teo Colomberotto <teo.colomberotto@heig-vd.ch>
2020-01-13 16:34:10 +0100

    search_triplist

--- src/app/home/search/search.page.ts
+++ src/app/home/search/search.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-search',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: [ './search.page.scss' ]
 })
 export class SearchPage implements OnInit {
-	constructor() {}
+	searchbar = document.querySelector('ion-searchbar');
+	trips: any = [];
+	searchTerm: any;
+	constructor(public http: HttpClient) {}
 
-	ngOnInit() {}
+	ngOnInit() {
+		const url = '/api/trips/';
+		this.http.get(url).subscribe((trips) => {
+			this.trips = trips;
+			console.log(`Trips loaded`, trips);
+		});
+	}
 	searchTermChange() {
 		console.log('tipped');
+		const query = event.target.value.toLowerCase();
+		requestAnimationFrame(() => {
+			this.trips.forEach((trip) => {
+				const shouldShow = trip.textContent.toLowerCase().indexOf(query) > -1;
+				trip.style.display = shouldShow ? 'block' : 'none';
+			});
+		});
 	}
 }
