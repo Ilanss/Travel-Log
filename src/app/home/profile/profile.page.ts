9b045f39b78deccfa4b98296cb46a60fffea359b
Skyggen <adrien.ciampone@heig-vd.ch>
2020-01-15 14:55:38 +0100

    Add websocket base

--- src/app/home/profile/profile.page.ts
+++ src/app/home/profile/profile.page.ts
@@ -1,24 +1,38 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
+import { WebsocketService } from 'src/app/services/websocket/websocket.service';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.page.html',
-  styleUrls: ['./profile.page.scss'],
+	selector: 'app-profile',
+	templateUrl: './profile.page.html',
+	styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage {
-
-  constructor(
+	message: string;
+	constructor(
 		// TODO: inject the authentication provider.
 		private auth: AuthService,
-		private router: Router
-	) {}
+		private router: Router,
+		private wsService: WebsocketService
+	) {
+		this.wsService
+		.listen()
+		.subscribe(message => {
+			// Do something when a message is received
+		});
+	}
 
 	// TODO: add a method to log out.
 	logOut() {
 		this.auth.logOut();
 		this.router.navigateByUrl('/login');
 	}
+
+	sendMessage() {
+		this.wsService.send({ msg: this.message });
+		console.log(this.message);
+	  }
+	
 
 }
